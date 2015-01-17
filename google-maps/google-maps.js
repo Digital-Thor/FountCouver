a62bf33fc803a0df3a92ddd46972d50493592824
jennylia <jennylian100@gmail.com>
2015-01-17 14:54:44 -0800

    try to plot a series of points

--- google-maps/google-maps.js
+++ google-maps/google-maps.js
@@ -1,3 +1,5 @@
+Locations = new Mongo.Collections("points");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
@@ -18,16 +20,25 @@ if (Meteor.isClient) {
   });
 
   Template.body.helpers({
-  exampleMapOptions: function() {
+    exampleMapOptions2: function() {
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
       // We can use the `ready` callback to interact with the map API once the map is ready.
       GoogleMaps.ready('exampleMap', function(map) {
         // Add a marker to the map once it's ready
-        var marker = new google.maps.Marker({
-          position: map.options.center,
-          map: map.instance
-        });
+        
+        //for loop to create markers
+        // var marker = new google.maps.Marker({
+        //   position: map.options.center,
+        //   map: map.instance
+        // });
+
+        // map = map.instance;
+        // for (int i = 0; i< Locations.find().count(); i++){
+        //   var marker = new google.maps.Marker({
+        //    position: Locations.,
+        //    map: map
+        // }
       });
 
       // Map initialization options
@@ -39,7 +50,7 @@ if (Meteor.isClient) {
   }
 });
 
-   Meteor.startup(function() {
+  Meteor.startup(function() {
     GoogleMaps.load();
   });
 }
@@ -47,6 +58,28 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Locations.find().count() == 0){
+      Locations.insert({
+        Lat: 49, 
+        Lon: -122.1
+      });
+
+      Locations.insert({
+        Lat: 49, 
+        Lon: -122.2
+      });
+
+      Locations.insert({
+        Lat: 48, 
+        Lon: -122.3
+      });
+
+      Locations.insert({
+        Lat: 48, 
+        Lon: -122.4
+      });
+    }
+
   });
 }
 
