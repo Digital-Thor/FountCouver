8403f330e004655bb2040ea0b5ae6b28ec0a8e0b
Thor Berggren <thor@interfusion.ca>
2015-01-21 23:01:24 -0800

    Working version. Still requires better timing to link ftp completion to parsing and client process.

--- google-maps/google-maps.js
+++ google-maps/google-maps.js
@@ -3,23 +3,7 @@ Locations = new Mongo.Collection("location");
 
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-
-
-  });
 
   Template.body.helpers({
     exampleMapOptions2: function() {
@@ -34,43 +18,41 @@ if (Meteor.isClient) {
         //   position: map.options.center,
         //   map: map.instance
         // });
-
-
-
-        var mapPoints = Locations.find();
-        //  debugger;
-
-        mapPoints.forEach(function (location) {
-          console.log(location.Lat, location.Lon);
-          // console.dir(location);
-            var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(location.Lat, location.Lon),
-            map: map.instance
-          });
-        });  
-
+        var clientDelay = 0;
+        if (Locations.find().count() === 0) clientDelay = 14000;
+        Meteor.setTimeout( function() {
+          var mapPoints = Locations.find();
+          //  debugger;
+          mapPoints.forEach(function (location) {
+            console.log(location.Lat, location.Lon);
+            // console.dir(location);
+              var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(location.Lat, location.Lon),
+              map: map.instance
+            });
+          });   // end mapPoints
+        },clientDelay);
       });
 
       // Map initialization options
       return {
-        center: new google.maps.LatLng(49, -122.9631),
-        zoom: 8
+        center: new google.maps.LatLng(49.2701294, -123.104467),
+        zoom: 12
       };
     }
   }
-
-
-});
+});  // end body helpers
 
   Meteor.startup(function() {
     GoogleMaps.load();
   });
-}
+} // end client code
 
 if (Meteor.isServer) {
 
-// Approach Source: NPM/jsftp documentation
-// Notes: data does show up, but not converting to ejson
+  // Approach:  NPM/jsftp documentation
+  // Link:      https://www.npmjs.com/package/jsftp
+  // Notes:     data does show up, but not converting to ejson
 
   Meteor.startup(function () {
     // code to run on server at startup
@@ -78,51 +60,40 @@ if (Meteor.isServer) {
     var Jsftp = Meteor.npmRequire('jsftp');
     var Ftp = new Jsftp({
       host: "webftp.vancouver.ca",
-      debugMode: true,
+      // debugMode: true,
       port:21
       });
-    var results = ""; // Will store the contents of the file
+
     Ftp.on('jsftp_debug', function(eventType, data) {
       console.log('DEBUG: ', eventType);
       console.log(JSON.stringify(data, null, 2));
     });
+
+    var results = ""; // Will store the contents of the file
+
     Ftp.get("/OpenData/json/drinking_fountains.json", function(err, socket) {
       if (err) return;
 
       socket.on("data", function(d) { 
+        console.log("* * * building results * * *");
         results += d.toString(); 
-        // console.log(results);
-        // debugger;
-
-        try {
-          ejsonObj = EJSON.parse(results);
-          }
-        catch(e){
-           console.log(e.name + " " + e.message)
-           }      
-        // debugger;
-        /* var features = (ejsonObj.features);
-        features.forEach(LoadLocations); */
         });
       socket.on("close", function(hadErr) {
+        console.log("Closed");
+        // console.log("*** results ****\n",results);
         if (hadErr)
           console.error('There was an error retrieving the file.');
       });
       socket.resume(console.log("Resuming Socket"));
     });
 
+
     Locations.remove({});  // remove any old locations in the database, ie: the old data we hard-coded into Locations.
 
-    // var url = "webftp.vancouver.ca/OpenData/json/drinking_fountains.json";
-    // var results = HTTP.get(url);
-
-    // results = Assets.getText("results.json");
-
-    // console.dir(results);
 
 
     function LoadLocations( coordpairArr ) {
-     // console.log(" Arr=", coordpairArr.geometry.coordinates[0], coordpairArr.geometry.coordinates[1]);
+     console.log(" Arr=", coordpairArr.geometry.coordinates[0], coordpairArr.geometry.coordinates[1]);
       Locations.insert({
         Lat: coordpairArr.geometry.coordinates[1], 
         Lon: coordpairArr.geometry.coordinates[0]
@@ -130,8 +101,20 @@ if (Meteor.isServer) {
       }
 
 
-  });
+  Meteor.setTimeout( function() {
+    console.log("About to parse results into EJSON");
+    try {
+      ejsonObj = EJSON.parse(results);
+      }
+    catch(e){
+       console.log("EJSON parsing error = " + e.name + " - " + e.message)
+       }    
+    var features = (ejsonObj.features);
+    features.forEach(LoadLocations);
+    }, 12000);
 
-}
+  });  // end server startup code
+
+}     // end server code
 
 
